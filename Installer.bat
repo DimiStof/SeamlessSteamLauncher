c32dfb269a2c1f0fd846932554251ba6236697fb
Dimi <131925563+DimiStof@users.noreply.github.com>
2026-05-07 23:03:19 +1000

    Update Installer.bat
    idk how but something broke but we are so back with timing everything seamlessly (at least on my system this stuff finnicky as hell)

--- Installer.bat
+++ Installer.bat
@@ -1,11 +1,24 @@
 @echo off
 
-::Script to force run as Admin
+:: Script to force run as Admin
 set "params=%*"
-cd /d "%~dp0" && ( if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs" && "%temp%\getadmin.vbs" && exit /B )
+cd /d "%~dp0" && (
+    if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs"
+) && fsutil dirty query %systemdrive% 1>nul 2>nul || (
+    echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/c cd ""%~sdp0"" && ""%~s0"" %params%", "", "runas", 0 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+)
 
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" /v Shell /t REG_SZ /d "wscript.exe \"%~dp0Steam\LauncherStartup.vbs\"" /f
+reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" ^
+/v Shell /t REG_SZ ^
+/d "wscript.exe \"%~dp0Steam\LauncherStartup.vbs\"" /f
 
+:: Success popup for 1 second
+echo Set WshShell = CreateObject("WScript.Shell") > "%temp%\success.vbs"
+echo WshShell.Popup "Operation Successful", 1, "Done", 64 >> "%temp%\success.vbs"
+wscript.exe "%temp%\success.vbs"
 
-timeout /t 3
+del "%temp%\success.vbs"
+
 exit
